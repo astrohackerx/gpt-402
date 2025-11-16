27961eac68304c217bb429df9ddad164b74fa259
astrohacker <astrohackerx@protonmail.com>
2025-11-16 18:58:11 +0000

    upd cors

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || '*',
+  origin: '*',
   credentials: true,
   exposedHeaders: ['X-Payment-Required']
 }));
